4ece20c832cb4dd9760ef075b39e0ee5bbf5ba19
ZeqiangLai <916323301@qq.com>
2025-03-22 22:41:34 +0800

    update

--- hy3dgen/text2image.py
+++ hy3dgen/text2image.py
@@ -1,13 +1,3 @@
-# Open Source Model Licensed under the Apache License Version 2.0
-# and Other Licenses of the Third-Party Components therein:
-# The below Model in this distribution may have been modified by THL A29 Limited
-# ("Tencent Modifications"). All Tencent Modifications are Copyright (C) 2024 THL A29 Limited.
-
-# Copyright (C) 2024 THL A29 Limited, a Tencent company.  All rights reserved.
-# The below software and/or models in this distribution may have been
-# modified by THL A29 Limited ("Tencent Modifications").
-# All Tencent Modifications are Copyright (C) THL A29 Limited.
-
 # Hunyuan 3D is licensed under the TENCENT HUNYUAN NON-COMMERCIAL LICENSE AGREEMENT
 # except for the third-party components listed below.
 # Hunyuan 3D does not impose any additional limitations beyond what is outlined
@@ -21,7 +11,6 @@
 # optimizer states), machine-learning model code, inference-enabling code, training-enabling code,
 # fine-tuning enabling code and other elements of the foregoing made publicly available
 # by Tencent in accordance with TENCENT HUNYUAN COMMUNITY LICENSE AGREEMENT.
-
 
 import os
 import random
@@ -80,9 +69,9 @@ class HunyuanDiTPipeline:
         generator = torch.Generator(device=self.pipe.device)
         generator = generator.manual_seed(int(seed))
         out_img = self.pipe(
-            prompt=self.pos_txt+prompt,
+            prompt=prompt[:60] + self.pos_txt,
             negative_prompt=self.neg_txt,
-            num_inference_steps=20,
+            num_inference_steps=25,
             pag_scale=1.3,
             width=1024,
             height=1024,
